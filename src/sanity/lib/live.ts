de4ecb49bc7cbebf9ed18ffd04db83d84c50643f
Abdelrhman <ph.abdelrhman84@gmail.com>
2026-05-07 17:35:03 +0300

    Fix build error by disabling unused live content API

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,16 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
+// Live content API configuration
+// This is currently disabled to resolve build errors with next-sanity v12
+/*
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
     apiVersion: 'vX' 
   }) 
 });
+*/
+
+// Temporary placeholders to prevent import errors
+export const sanityFetch = () => {};
+export const SanityLive = () => null;
