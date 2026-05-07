7367f8c7eff769a4fc225bded05006e8040b34f6
Abdelrhman <ph.abdelrhman84@gmail.com>
2026-05-07 17:48:11 +0300

    Allow Sanity to iframe the site for Studio preview

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,19 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://*.sanity.io https://sanity.io",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
